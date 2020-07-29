9b7780dcf36d22d8021ac7d10053dc1cf0501329
Folker Kinzel <fkinzel@freenet.de>
2020-07-29 13:59:10 +0200

    ChangeSourceDocumentCommand

--- Tsltn/Commands/TsltnCommand.cs
+++ Tsltn/Commands/TsltnCommand.cs
@@ -9,5 +9,6 @@ namespace Tsltn.Commands
     public static class TsltnCommand
     {
         public static RoutedCommand Translate { get; } = new RoutedCommand("Translate", typeof(TsltnCommand));
+        public static RoutedCommand ChangeSourceDocument { get; } = new RoutedCommand("ChangeSourceDocument", typeof(TsltnCommand));
     }
 }
