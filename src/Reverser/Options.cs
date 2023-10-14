1d5903c31c1b974a8838a713fcdc5c10404a9bea
Folker Kinzel <fkinzel@freenet.de>
2023-10-14 14:31:05 +0200

    work

---
+++ src/Reverser/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverser;
+
+internal class Options
+{
+    [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+    public bool Verbose { get; set; }
+
+
+    [Value(0, Required = false, HelpText = "Path to the cofiguration file.")]
+    public string? ConfigPath { get; set; }
+
+
+    [Option("m", Default = "production", Required = false, HelpText = "Enables to run in test mode.")]
+    public string? Mode { get; set; }
+}
---
+++ src/Reverser/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+using Reverser;
+
+Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
